2da2f7814ce53d04826e0987ee226b1034308296
omid shahini <moshahini@yahoo.com>
2025-02-20 19:54:29 +0330

    js codes added

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 
+let userElem = document.querySelector('.username')
+let passElem = document.querySelector('.password')
+let modalElem = document.querySelector('.modal')
+
+let userElemValue = userElem.value
+let passElemValue = passElem.value
+
+function checkValid () {
+  if (userElemValue.length < 12 || passElemValue.length < 8) {
+    modalElem.style.display = 'inline'
+
+    setTimeout(function () {
+      modalElem.style.display = 'none'
+    }, 2000)
+  }
+}
